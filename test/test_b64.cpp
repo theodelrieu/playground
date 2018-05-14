6f1661d9cf15821012020f5aa4abda3c00f5dc57
Théo DELRIEU <theo.delrieu@tanker.io>
2018-05-14 14:24:04 +0200

    put back regular test

--- test/test_b64.cpp
+++ test/test_b64.cpp
@@ -1,7 +1,7 @@
 #include <algorithm>
-#include <iostream>
 #include <chrono>
 #include <fstream>
+#include <iostream>
 #include <iterator>
 #include <type_traits>
 #include <vector>
@@ -19,7 +19,9 @@ namespace
 // streams are not Iterable until C++20.
 struct stream_iterable_adapter
 {
-  stream_iterable_adapter(std::istream& is): _is(is){}
+  stream_iterable_adapter(std::istream& is) : _is(is)
+  {
+  }
 
   auto begin() const
   {
@@ -76,39 +78,8 @@ TEST_CASE("b64 encode", "[b64]")
     stream_iterable_adapter input{random_data};
     b64::encoders::base64_stream_encoder<decltype(input.begin())> pr(
         input.begin(), input.end());
-    // std::istreambuf_iterator<char> expectedB64It(b64_random_data);
-    // std::istreambuf_iterator<char> expectedEnd;
-    auto p = std::chrono::steady_clock::now();
-    std::vector<char> b64v(pr.begin(), pr.end());
-    auto p2 = std::chrono::steady_clock::now();
-    std::cout
-        << "Conversion took "
-        << std::chrono::duration_cast<std::chrono::milliseconds>(p2 - p).count()
-        << "ms" << std::endl;
-    // CHECK(std::equal(expectedB64It, expectedEnd, pr.begin(), pr.end()));
-  }
-
-  SECTION("huge file contiguous")
-  {
-    REQUIRE(testFilePaths.size() == 2);
-    std::ifstream random_data(testFilePaths[0]);
-    std::ifstream b64_random_data(testFilePaths[1]);
-    stream_iterable_adapter input{random_data};
-    std::vector<std::uint8_t> v(input.begin(), input.end());
-    b64::encoders::base64_stream_encoder<decltype(v.begin())> pr(v.begin(),
-                                                                 v.end());
-
     std::istreambuf_iterator<char> expectedB64It(b64_random_data);
     std::istreambuf_iterator<char> expectedEnd;
-    std::vector<char> expected(expectedB64It, expectedEnd);
-
-    auto p = std::chrono::steady_clock::now();
-    std::vector<char> b64v(pr.begin(), pr.end());
-    auto p2 = std::chrono::steady_clock::now();
-    std::cout
-        << "Conversion took "
-        << std::chrono::duration_cast<std::chrono::milliseconds>(p2 - p).count()
-        << "ms" << std::endl;
-    CHECK(b64v == expected);
+    CHECK(std::equal(expectedB64It, expectedEnd, pr.begin(), pr.end()));
   }
 }
