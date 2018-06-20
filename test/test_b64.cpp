476ec10507b84412983c4202dfe8a17362118902
Théo DELRIEU <theo.delrieu@tanker.io>
2018-06-20 18:13:30 +0200

    wip

--- test/test_b64.cpp
+++ test/test_b64.cpp
@@ -26,21 +26,21 @@ extern std::vector<std::string> testFilePaths;
 namespace
 {
 template <typename Iterator, typename Sentinel = Iterator>
-using encoder = detail::base64_lazy_encoder<Iterator, Sentinel>;
+using b64_encoder = detail::base64_lazy_encoder<Iterator, Sentinel>;
 
 template <typename Iterator, typename Sentinel = Iterator>
-using decoder = detail::base64_lazy_decoder<Iterator, Sentinel>;
+using b64_decoder = detail::base64_lazy_decoder<Iterator, Sentinel>;
 
 static_assert(
-    detail::is_iterable_input_transformer<encoder<char*>>::value, "");
+    detail::is_iterable_input_transformer<b64_encoder<char*>>::value, "");
 static_assert(detail::is_iterable_input_transformer<
-                  encoder<std::list<char>::iterator>>::value,
+                  b64_encoder<std::list<char>::iterator>>::value,
               "");
 static_assert(detail::is_iterable_input_transformer<
-                  encoder<std::forward_list<char>::iterator>>::value,
+                  b64_encoder<std::forward_list<char>::iterator>>::value,
               "");
 static_assert(detail::is_iterable_input_transformer<
-                  encoder<std::istreambuf_iterator<char>>>::value,
+                  b64_encoder<std::istreambuf_iterator<char>>>::value,
               "");
 
 struct encode_tag
@@ -101,10 +101,8 @@ static_assert(
 static_assert(
     !detail::is_sentinel<sentinel, std::vector<char>::iterator>::value, "");
 
-template <template <typename, typename, typename...> class InputTransformer,
-          typename Container,
-          typename Iterable>
-void base64_checks_impl(Container const& source,
+template <typename EncodingTraits, typename Container, typename Iterable>
+void base_n_checks_impl(Container const& source,
                         Iterable const& expected_output)
 {
   using Iterator = detail2::result_of_begin_t<Container>;
@@ -113,7 +111,9 @@ void base64_checks_impl(Container const& source,
   using std::begin;
   using std::end;
 
-  InputTransformer<Iterator, Sentinel> transformer(begin(source), end(source));
+  detail::base_n_transformer<EncodingTraits, Iterator, Sentinel> transformer(
+      begin(source), end(source));
+
   std::vector<std::uint8_t> const output(transformer.begin(),
                                          transformer.end());
   std::vector<std::uint8_t> const expected(expected_output.begin(),
@@ -122,90 +122,52 @@ void base64_checks_impl(Container const& source,
   CHECK(output == expected);
 }
 
-template <typename Container, typename Iterable>
-void base64_checks_impl(Container const& source,
-                        Iterable const& expected_output,
-                        encode_tag)
-{
-  base64_checks_impl<encoder>(source, expected_output);
-}
-
-template <typename Container, typename Iterable>
-void base64_checks_impl(Iterable const& expected_output,
-                        Container const& source,
-                        decode_tag)
-{
-  base64_checks_impl<decoder>(source, expected_output);
-}
-
-template <typename Container,
+template <typename EncodingTraits,
+          typename Container,
           typename Iterable,
-          typename Iterable2,
-          typename Tag>
-void base64_checks(Iterable const& source, Iterable2 const& expected, Tag tag)
+          typename Iterable2>
+void base_n_checks(Iterable const& source, Iterable2 const& expected)
 {
   using std::begin;
   using std::end;
 
   Container cont(begin(source), end(source));
 
-  base64_checks_impl(cont, expected, tag);
+  base_n_checks_impl<EncodingTraits>(cont, expected);
 }
 
-template <typename Tag>
-void common_checks()
+template <typename EncodingTraits,
+          typename Container,
+          typename Input,
+          typename Output>
+void base_n_checks(std::vector<Input> const& inputs,
+                   std::vector<Output> const& outputs)
 {
-  Tag tag;
+  REQUIRE(inputs.size() == outputs.size());
 
-  std::array<char, 4> decoded_two_byte_padding = {'a', 'b', 'c', 'd'};
-  std::array<char, 5> decoded_one_byte_padding = {'a', 'b', 'c', 'd', 'e'};
-  std::array<char, 6> decoded_no_padding = {'a', 'b', 'c', 'd', 'e', 'f'};
+  for (auto i = 0; i < inputs.size(); ++i)
+    base_n_checks<EncodingTraits, Container>(inputs[i], outputs[i]);
+}
 
-  std::array<char, 8> const encoded_two_byte_padding{
-      'Y', 'W', 'J', 'j', 'Z', 'A', '=', '='};
-  std::array<char, 8> const encoded_one_byte_padding{
-      'Y', 'W', 'J', 'j', 'Z', 'G', 'U', '='};
-  std::array<char, 8> const encoded_no_padding{
-      'Y', 'W', 'J', 'j', 'Z', 'G', 'V', 'm'};
-
+template <typename EncodingTraits, typename Input, typename Output>
+void common_checks(std::vector<Input> const& inputs,
+                   std::vector<Output> const& outputs)
+{
   SECTION("RandomAccessIterator")
   {
     SECTION("std::string")
     {
-      base64_checks<std::string>(
-          decoded_two_byte_padding, encoded_two_byte_padding, tag);
-      base64_checks<std::string>(
-          decoded_one_byte_padding, encoded_one_byte_padding, tag);
-      base64_checks<std::string>(decoded_no_padding, encoded_no_padding, tag);
+      base_n_checks<EncodingTraits, std::string>(inputs, outputs);
     }
 
     SECTION("std::vector")
     {
-      base64_checks<std::vector<char>>(
-          decoded_two_byte_padding, encoded_two_byte_padding, tag);
-      base64_checks<std::vector<char>>(
-          decoded_one_byte_padding, encoded_one_byte_padding, tag);
-      base64_checks<std::vector<char>>(
-          decoded_no_padding, encoded_no_padding, tag);
+      base_n_checks<EncodingTraits, std::vector<char>>(inputs, outputs);
     }
 
     SECTION("std::deque")
     {
-      base64_checks<std::deque<char>>(
-          decoded_two_byte_padding, encoded_two_byte_padding, tag);
-      base64_checks<std::deque<char>>(
-          decoded_one_byte_padding, encoded_one_byte_padding, tag);
-      base64_checks<std::deque<char>>(
-          decoded_no_padding, encoded_no_padding, tag);
-    }
-
-    SECTION("std::array")
-    {
-      base64_checks_impl(
-          decoded_two_byte_padding, encoded_two_byte_padding, tag);
-      base64_checks_impl(
-          decoded_one_byte_padding, encoded_one_byte_padding, tag);
-      base64_checks_impl(decoded_no_padding, encoded_no_padding, tag);
+      base_n_checks<EncodingTraits, std::deque<char>>(inputs, outputs);
     }
   }
 
@@ -213,40 +175,15 @@ void common_checks()
   {
     SECTION("std::list")
     {
-      base64_checks<std::list<char>>(
-          decoded_two_byte_padding, encoded_two_byte_padding, tag);
-      base64_checks<std::list<char>>(
-          decoded_one_byte_padding, encoded_one_byte_padding, tag);
-      base64_checks<std::list<char>>(
-          decoded_no_padding, encoded_no_padding, tag);
+      base_n_checks<EncodingTraits, std::list<char>>(inputs, outputs);
     }
   }
 
   SECTION("ForwardIterator")
   {
     SECTION("std::forward_list")
-    {
-      base64_checks<std::forward_list<char>>(
-          decoded_two_byte_padding, encoded_two_byte_padding, tag);
-      base64_checks<std::forward_list<char>>(
-          decoded_one_byte_padding, encoded_one_byte_padding, tag);
-      base64_checks<std::forward_list<char>>(
-          decoded_no_padding, encoded_no_padding, tag);
-    }
-  }
-
-  SECTION("InputIterator")
-  {
-    SECTION("std::ifstream")
-    {
-      REQUIRE(testFilePaths.size() == 2);
-      std::ifstream random_data(testFilePaths[0]);
-      std::ifstream b64_random_data(testFilePaths[1]);
-
-      stream_iterable_adapter random_input{random_data};
-      stream_iterable_adapter b64_random_input{b64_random_data};
-
-      base64_checks_impl(random_input, b64_random_input, tag);
+    { 
+      base_n_checks<EncodingTraits, std::forward_list<char>>(inputs, outputs);
     }
   }
 }
@@ -254,11 +191,16 @@ void common_checks()
 
 TEST_CASE("b64 lazy", "[base64]")
 {
+  std::vector<std::string> decoded{"abcd"s, "abcde"s, "abcdef"s};
+  std::vector<std::string> encoded{"YWJjZA=="s, "YWJjZGU="s, "YWJjZGVm"s};
+
   SECTION("encoding")
   {
+    using Traits = detail::base64_encode_traits;
+
     SECTION("common_checks")
     {
-      common_checks<encode_tag>();
+      common_checks<Traits>(decoded, encoded);
     }
 
     SECTION("sentinel")
@@ -266,7 +208,7 @@ TEST_CASE("b64 lazy", "[base64]")
       std::stringstream ss{"abcde"s};
       auto const b64_text = "YWJjZGU="s;
 
-      encoder<std::istreambuf_iterator<char>, sentinel> enc(
+      b64_encoder<std::istreambuf_iterator<char>, sentinel> enc(
           std::istreambuf_iterator<char>(ss), sentinel{});
 
       std::string s(enc.begin(), enc.end());
@@ -279,8 +221,8 @@ TEST_CASE("b64 lazy", "[base64]")
       auto const b64_text = "YWJjZGU="s;
       auto const final_b64_text = "WVdKalpHVT0="s;
 
-      encoder<std::string::const_iterator> first_enc(text.begin(), text.end());
-      encoder<decltype(first_enc.begin())> second_enc(first_enc.begin(),
+      b64_encoder<std::string::const_iterator> first_enc(text.begin(), text.end());
+      b64_encoder<decltype(first_enc.begin())> second_enc(first_enc.begin(),
                                                       first_enc.end());
 
       std::string s(second_enc.begin(), second_enc.end());
@@ -290,9 +232,11 @@ TEST_CASE("b64 lazy", "[base64]")
 
   SECTION("decoding")
   {
+    using Traits = detail::base64_decode_traits;
+
     SECTION("common_checks")
     {
-      common_checks<decode_tag>();
+      common_checks<Traits>(encoded, decoded);
     }
 
     SECTION("sentinel")
@@ -300,7 +244,7 @@ TEST_CASE("b64 lazy", "[base64]")
       std::stringstream ss{"YWJjZGU="s};
       auto const text = "abcde"s;
 
-      decoder<std::istreambuf_iterator<char>, sentinel> dec(
+      b64_decoder<std::istreambuf_iterator<char>, sentinel> dec(
           std::istreambuf_iterator<char>(ss), sentinel{});
 
       std::string s(dec.begin(), dec.end());
@@ -313,9 +257,9 @@ TEST_CASE("b64 lazy", "[base64]")
       auto const b64_text = "YWJjZGU="s;
       auto const final_b64_text = "WVdKalpHVT0="s;
 
-      decoder<std::string::const_iterator> first_dec(final_b64_text.begin(),
+      b64_decoder<std::string::const_iterator> first_dec(final_b64_text.begin(),
                                                      final_b64_text.end());
-      decoder<decltype(first_dec.begin())> second_dec(first_dec.begin(),
+      b64_decoder<decltype(first_dec.begin())> second_dec(first_dec.begin(),
                                                       first_dec.end());
 
       std::string s(second_dec.begin(), second_dec.end());
@@ -329,8 +273,8 @@ TEST_CASE("b64 lazy", "[base64]")
     {
       auto const text = "abcde"s;
 
-      encoder<std::string::const_iterator> enc(text.begin(), text.end());
-      decoder<decltype(enc.begin())> dec(enc.begin(), enc.end());
+      b64_encoder<std::string::const_iterator> enc(text.begin(), text.end());
+      b64_decoder<decltype(enc.begin())> dec(enc.begin(), enc.end());
 
       std::string s(dec.begin(), dec.end());
       CHECK(s == text);
@@ -340,11 +284,27 @@ TEST_CASE("b64 lazy", "[base64]")
     {
       auto const text = "YWJjZGU="s;
 
-      decoder<std::string::const_iterator> dec(text.begin(), text.end());
-      encoder<decltype(dec.begin())> enc(dec.begin(), dec.end());
+      b64_decoder<std::string::const_iterator> dec(text.begin(), text.end());
+      b64_encoder<decltype(dec.begin())> enc(dec.begin(), dec.end());
 
       std::string s(enc.begin(), enc.end());
       CHECK(s == text);
     }
+  }
+
+  SECTION("stream")
+  {
+    REQUIRE(testFilePaths.size() == 2);
+    std::ifstream random_data(testFilePaths[0]);
+    std::ifstream b64_random_data(testFilePaths[1]);
+
+    stream_iterable_adapter random_input{random_data};
+    stream_iterable_adapter b64_random_input{b64_random_data};
+
+    using EncoderTraits = detail::base64_encode_traits;
+    using DecoderTraits = detail::base64_encode_traits;
+
+    base_n_checks_impl<EncoderTraits>(random_input, b64_random_input);
+    base_n_checks_impl<DecoderTraits>(b64_random_input, random_input);
   }
 }
