35134b94e76ac49a91157e4e28050f87eb776f6e
Théo DELRIEU <theo.delrieu@tanker.io>
2018-06-21 17:17:09 +0200

    wip refactor tests

--- test/test_b64.cpp
+++ test/test_b64.cpp
@@ -13,10 +13,6 @@
 
 #include <mgs/detail/base64/lazy_encoder.hpp>
 #include <mgs/detail/base64/lazy_decoder.hpp>
-#include <mgs/detail/base32/lazy_encoder.hpp>
-#include <mgs/detail/base32/lazy_decoder.hpp>
-#include <mgs/detail/base16/lazy_encoder.hpp>
-#include <mgs/detail/base16/lazy_decoder.hpp>
 #include <mgs/detail/meta/concepts/derived_from.hpp>
 #include <mgs/detail/meta/concepts/iterable.hpp>
 #include <mgs/detail/meta/concepts/iterable_input_transformer.hpp>
@@ -25,10 +21,6 @@
 using namespace std::string_literals;
 using namespace mgs;
 
-extern std::vector<std::string> testFilePaths;
-
-namespace
-{
 template <typename Iterator, typename Sentinel = Iterator>
 using b64_encoder = detail::base64_lazy_encoder<Iterator, Sentinel>;
 
@@ -47,152 +39,6 @@ static_assert(detail::is_iterable_input_transformer<
                   b64_encoder<std::istreambuf_iterator<char>>>::value,
               "");
 
-struct encode_tag
-{
-};
-
-struct decode_tag
-{
-};
-
-// streams are not Iterable until C++20.
-struct stream_iterable_adapter
-{
-  stream_iterable_adapter(std::istream& is) : _is(is)
-  {
-  }
-
-  auto begin() const
-  {
-    return std::istreambuf_iterator<char>{_is};
-  }
-
-  auto end() const
-  {
-    return std::istreambuf_iterator<char>{};
-  }
-
-  std::istream& _is;
-};
-
-struct sentinel
-{
-};
-
-bool operator==(std::istreambuf_iterator<char> lhs, sentinel) noexcept
-{
-  return lhs.equal({});
-}
-
-bool operator==(sentinel, std::istreambuf_iterator<char> rhs) noexcept
-{
-  return rhs.equal({});
-}
-
-bool operator!=(std::istreambuf_iterator<char> lhs, sentinel s) noexcept
-{
-  return !(lhs == s);
-}
-
-bool operator!=(sentinel s, std::istreambuf_iterator<char> rhs) noexcept
-{
-  return !(s == rhs);
-}
-
-static_assert(
-    detail::is_sentinel<sentinel, std::istreambuf_iterator<char>>::value, "");
-
-static_assert(
-    !detail::is_sentinel<sentinel, std::vector<char>::iterator>::value, "");
-
-template <typename EncodingTraits, typename Container, typename Iterable>
-void base_n_checks_impl(Container const& source,
-                        Iterable const& expected_output)
-{
-  using Iterator = detail2::result_of_begin_t<Container>;
-  using Sentinel = detail2::result_of_end_t<Container>;
-
-  using std::begin;
-  using std::end;
-
-  detail::base_n_transformer<EncodingTraits, Iterator, Sentinel> transformer(
-      begin(source), end(source));
-
-  std::vector<std::uint8_t> const output(transformer.begin(),
-                                         transformer.end());
-  std::vector<std::uint8_t> const expected(expected_output.begin(),
-                                           expected_output.end());
-
-  CHECK(output == expected);
-}
-
-template <typename EncodingTraits,
-          typename Container,
-          typename Iterable,
-          typename Iterable2>
-void base_n_checks(Iterable const& source, Iterable2 const& expected)
-{
-  using std::begin;
-  using std::end;
-
-  Container cont(begin(source), end(source));
-
-  base_n_checks_impl<EncodingTraits>(cont, expected);
-}
-
-template <typename EncodingTraits,
-          typename Container,
-          typename Input,
-          typename Output>
-void base_n_checks(std::vector<Input> const& inputs,
-                   std::vector<Output> const& outputs)
-{
-  REQUIRE(inputs.size() == outputs.size());
-
-  for (auto i = 0; i < inputs.size(); ++i)
-    base_n_checks<EncodingTraits, Container>(inputs[i], outputs[i]);
-}
-
-template <typename EncodingTraits, typename Input, typename Output>
-void common_checks(std::vector<Input> const& inputs,
-                   std::vector<Output> const& outputs)
-{
-  SECTION("RandomAccessIterator")
-  {
-    SECTION("std::string")
-    {
-      base_n_checks<EncodingTraits, std::string>(inputs, outputs);
-    }
-
-    SECTION("std::vector")
-    {
-      base_n_checks<EncodingTraits, std::vector<char>>(inputs, outputs);
-    }
-
-    SECTION("std::deque")
-    {
-      base_n_checks<EncodingTraits, std::deque<char>>(inputs, outputs);
-    }
-  }
-
-  SECTION("BidirectionalIterator")
-  {
-    SECTION("std::list")
-    {
-      base_n_checks<EncodingTraits, std::list<char>>(inputs, outputs);
-    }
-  }
-
-  SECTION("ForwardIterator")
-  {
-    SECTION("std::forward_list")
-    { 
-      base_n_checks<EncodingTraits, std::forward_list<char>>(inputs, outputs);
-    }
-  }
-}
-}
-
 TEST_CASE("b64 lazy", "[base64]")
 {
   std::vector<std::string> decoded{"abcd"s, "abcde"s, "abcdef"s};
@@ -209,28 +55,12 @@ TEST_CASE("b64 lazy", "[base64]")
 
     SECTION("sentinel")
     {
-      std::stringstream ss{"abcde"s};
-      auto const b64_text = "YWJjZGU="s;
-
-      b64_encoder<std::istreambuf_iterator<char>, sentinel> enc(
-          std::istreambuf_iterator<char>(ss), sentinel{});
-
-      std::string s(enc.begin(), enc.end());
-      CHECK(s == b64_text);
+      sentinel_check<Traits>("abcde"s, "YWJjZGU="s);
     }
 
     SECTION("Inception")
     {
-      auto const text = "abcde"s;
-      auto const b64_text = "YWJjZGU="s;
-      auto const final_b64_text = "WVdKalpHVT0="s;
-
-      b64_encoder<std::string::const_iterator> first_enc(text.begin(), text.end());
-      b64_encoder<decltype(first_enc.begin())> second_enc(first_enc.begin(),
-                                                      first_enc.end());
-
-      std::string s(second_enc.begin(), second_enc.end());
-      CHECK(s == final_b64_text);
+      inception_check("abcde"s, "YWJjZGU="s, "WVdKalpHVT0="s);
     }
   }
 
@@ -245,57 +75,31 @@ TEST_CASE("b64 lazy", "[base64]")
 
     SECTION("sentinel")
     {
-      std::stringstream ss{"YWJjZGU="s};
-      auto const text = "abcde"s;
-
-      b64_decoder<std::istreambuf_iterator<char>, sentinel> dec(
-          std::istreambuf_iterator<char>(ss), sentinel{});
-
-      std::string s(dec.begin(), dec.end());
-      CHECK(s == text);
+      sentinel_check<Traits>("YWJjZGU="s, "abcde"s);
     }
 
     SECTION("Inception")
     {
-      auto const text = "abcde"s;
-      auto const b64_text = "YWJjZGU="s;
-      auto const final_b64_text = "WVdKalpHVT0="s;
-
-      b64_decoder<std::string::const_iterator> first_dec(final_b64_text.begin(),
-                                                     final_b64_text.end());
-      b64_decoder<decltype(first_dec.begin())> second_dec(first_dec.begin(),
-                                                      first_dec.end());
-
-      std::string s(second_dec.begin(), second_dec.end());
-      CHECK(s == text);
+      inception_check("WVdKalpHVT0="s, "YWJjZGU="s, "abcde"s);
     }
   }
 
   SECTION("back and forth")
   {
+    using EncoderTraits = detail::base64_encode_traits;
+    using DecoderTraits = detail::base64_decode_traits;
+
     SECTION("decode(encode())")
     {
-      auto const text = "abcde"s;
-
-      b64_encoder<std::string::const_iterator> enc(text.begin(), text.end());
-      b64_decoder<decltype(enc.begin())> dec(enc.begin(), enc.end());
-
-      std::string s(dec.begin(), dec.end());
-      CHECK(s == text);
+      back_and_forth_check<EncoderTraits, DecoderTraits>("abcde"s);
     }
 
     SECTION("encode(decode())")
     {
-      auto const text = "YWJjZGU="s;
-
-      b64_decoder<std::string::const_iterator> dec(text.begin(), text.end());
-      b64_encoder<decltype(dec.begin())> enc(dec.begin(), dec.end());
-
-      std::string s(enc.begin(), enc.end());
-      CHECK(s == text);
+      back_and_forth_check<DecoderTraits, EncoderTraits>("YWJjZGU="s);
     }
   }
-
+// TODO do streams
   SECTION("stream")
   {
     REQUIRE(testFilePaths.size() == 2);
---
+++ test/test_base_n.hpp
@@ -0,0 +1,219 @@
+#pragma once
+
+#include <algorithm>
+#include <chrono>
+#include <deque>
+#include <forward_list>
+#include <fstream>
+#include <iterator>
+#include <list>
+#include <sstream>
+#include <type_traits>
+#include <vector>
+
+#include <catch.hpp>
+
+#include <mgs/detail/meta/concepts/derived_from.hpp>
+#include <mgs/detail/meta/concepts/iterable.hpp>
+#include <mgs/detail/meta/concepts/iterable_input_transformer.hpp>
+#include <mgs/exceptions/parse_error.hpp>
+
+using namespace std::string_literals;
+
+extern std::vector<std::string> testFilePaths;
+
+namespace mgs
+{
+// streams are not Iterable until C++20.
+struct stream_iterable_adapter
+{
+  stream_iterable_adapter(std::istream& is) : _is(is)
+  {
+  }
+
+  auto begin() const
+  {
+    return std::istreambuf_iterator<char>{_is};
+  }
+
+  auto end() const
+  {
+    return std::istreambuf_iterator<char>{};
+  }
+
+  std::istream& _is;
+};
+
+struct sentinel
+{
+};
+
+bool operator==(std::istreambuf_iterator<char> lhs, sentinel) noexcept
+{
+  return lhs.equal({});
+}
+
+bool operator==(sentinel, std::istreambuf_iterator<char> rhs) noexcept
+{
+  return rhs.equal({});
+}
+
+bool operator!=(std::istreambuf_iterator<char> lhs, sentinel s) noexcept
+{
+  return !(lhs == s);
+}
+
+bool operator!=(sentinel s, std::istreambuf_iterator<char> rhs) noexcept
+{
+  return !(s == rhs);
+}
+
+static_assert(
+    detail::is_sentinel<sentinel, std::istreambuf_iterator<char>>::value, "");
+
+static_assert(
+    !detail::is_sentinel<sentinel, std::vector<char>::iterator>::value, "");
+
+template <typename EncodingTraits, typename Container, typename Iterable>
+void base_n_checks_impl(Container const& source,
+                        Iterable const& expected_output)
+{
+  using Iterator = detail2::result_of_begin_t<Container>;
+  using Sentinel = detail2::result_of_end_t<Container>;
+
+  using std::begin;
+  using std::end;
+
+  detail::base_n_transformer<EncodingTraits, Iterator, Sentinel> transformer(
+      begin(source), end(source));
+
+  std::vector<std::uint8_t> const output(transformer.begin(),
+                                         transformer.end());
+  std::vector<std::uint8_t> const expected(expected_output.begin(),
+                                           expected_output.end());
+
+  CHECK(output == expected);
+}
+
+template <typename EncodingTraits,
+          typename Container,
+          typename Iterable,
+          typename Iterable2>
+void base_n_checks(Iterable const& source, Iterable2 const& expected)
+{
+  using std::begin;
+  using std::end;
+
+  Container cont(begin(source), end(source));
+
+  base_n_checks_impl<EncodingTraits>(cont, expected);
+}
+
+template <typename EncodingTraits,
+          typename Container,
+          typename Input,
+          typename Output>
+void base_n_checks(std::vector<Input> const& inputs,
+                   std::vector<Output> const& outputs)
+{
+  REQUIRE(inputs.size() == outputs.size());
+
+  for (auto i = 0; i < inputs.size(); ++i)
+    base_n_checks<EncodingTraits, Container>(inputs[i], outputs[i]);
+}
+
+template <typename EncodingTraits, typename Input, typename Output>
+void common_checks(std::vector<Input> const& inputs,
+                   std::vector<Output> const& outputs)
+{
+  SECTION("RandomAccessIterator")
+  {
+    SECTION("std::string")
+    {
+      base_n_checks<EncodingTraits, std::string>(inputs, outputs);
+    }
+
+    SECTION("std::vector")
+    {
+      base_n_checks<EncodingTraits, std::vector<char>>(inputs, outputs);
+    }
+
+    SECTION("std::deque")
+    {
+      base_n_checks<EncodingTraits, std::deque<char>>(inputs, outputs);
+    }
+  }
+
+  SECTION("BidirectionalIterator")
+  {
+    SECTION("std::list")
+    {
+      base_n_checks<EncodingTraits, std::list<char>>(inputs, outputs);
+    }
+  }
+
+  SECTION("ForwardIterator")
+  {
+    SECTION("std::forward_list")
+    { 
+      base_n_checks<EncodingTraits, std::forward_list<char>>(inputs, outputs);
+    }
+  }
+
+  SECTION("InputIterator")
+  {
+    REQUIRE(testFilePaths.size() == 2);
+    std::ifstream input(testFilePaths[0]);
+    std::ifstream output(testFilePaths[1]);
+
+    stream_iterable_adapter input_stream{input};
+    stream_iterable_adapter output_stream{output};
+
+    base_n_checks_impl<EncodingTraits>(input_stream, output_stream);
+  }
+}
+
+template <typename EncodingTraits>
+void sentinel_check(std::string const& input, std::string const& output)
+{
+  std::stringstream ss{input};
+
+  detail::base_n_transformer<EncodingTrait,
+                             std::istreambuf_iterator<char>,
+                             sentinel>
+  transformer(std::istreambuf_iterator<char>(ss), sentinel{});
+
+  std::string s(transformer.begin(), transformer.end());
+  CHECK(s == output);
+}
+
+template <typename EncodingTraits>
+void inception_check(std::string const& input,
+                     std::string const& first_output,
+                     std::string const& final_output)
+{
+  detail::base_n_transformer<EncodingTraits, std::string::const_iterator>
+      first_transformer(input.begin(), input.end());
+
+  detail::base_n_transformer<EncodingTraits,
+                             decltype(first_transformer.begin())>
+      second_transformer(first_transformer.begin(), first_transformer.end());
+
+  std::string s(second_transformer.begin(), second_transformer.end());
+  CHECK(s == final_output);
+}
+
+template <typename EncoderTraits, typename DecoderTraits>
+void back_and_forth_check(std::string const& input)
+{
+  detail::base_n_transformer<EncoderTraits, std::string::const_iterator> enc(
+      input.begin(), input.end());
+
+  detail::base_n_transformer<EncoderTraits, decltype(enc.begin())> dec(
+      enc.begin(), 
+      enc.end());
+
+  std::string s(dec.begin(), dec.end());
+  CHECK(s == input);
+}
+}
