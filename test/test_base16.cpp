18e83e91ff401b6ea5c61df3bc5f12bad3b4687f
Théo DELRIEU <theo.delrieu@tanker.io>
2018-06-28 11:50:47 +0200

    base16: invalid input tests

--- test/test_base16.cpp
+++ test/test_base16.cpp
@@ -57,53 +57,49 @@ TEST_CASE("b16 lazy", "[base16]")
       "666F6F626172"s,
   };
 
+  using EncoderTraits = detail::base16_encode_traits;
+  using DecoderTraits = detail::base16_decode_traits;
+
   SECTION("encoding")
   {
-    using Traits = detail::base16_encode_traits;
-
     SECTION("common_checks")
     {
-      common_checks<Traits>(decoded, encoded);
+      common_checks<EncoderTraits>(decoded, encoded);
     }
 
     SECTION("sentinel")
     {
-      sentinel_check<Traits>(decoded.back(), encoded.back());
+      sentinel_check<EncoderTraits>(decoded.back(), encoded.back());
     }
 
     SECTION("Inception")
     {
-      inception_check<Traits>(
+      inception_check<EncoderTraits>(
           decoded.back(), encoded.back(), "363636463646363236313732"s);
     }
   }
 
   SECTION("decoding")
   {
-    using Traits = detail::base16_decode_traits;
-
     SECTION("common_checks")
     {
-      common_checks<Traits>(encoded, decoded);
+      common_checks<DecoderTraits>(encoded, decoded);
     }
 
     SECTION("sentinel")
     {
-      sentinel_check<Traits>(encoded.back(), decoded.back());
+      sentinel_check<DecoderTraits>(encoded.back(), decoded.back());
     }
 
     SECTION("Inception")
     {
-      inception_check<Traits>(
+      inception_check<DecoderTraits>(
           "363636463646363236313732"s, encoded.back(), decoded.back());
     }
   }
 
   SECTION("back and forth")
   {
-    using EncoderTraits = detail::base16_encode_traits;
-    using DecoderTraits = detail::base16_decode_traits;
-
     SECTION("decode(encode())")
     {
       back_and_forth_check<EncoderTraits, DecoderTraits>(decoded.back());
@@ -121,10 +117,17 @@ TEST_CASE("b16 lazy", "[base16]")
     std::ifstream random_data(testFilePaths[0]);
     std::ifstream b16_random_data(testFilePaths[1]);
 
-    using EncoderTraits = detail::base16_encode_traits;
-    using DecoderTraits = detail::base16_encode_traits;
-
     stream_check<EncoderTraits>(random_data, b16_random_data);
     stream_check<DecoderTraits>(b16_random_data, random_data);
+  }
+
+  SECTION("invalid input")
+  {
+    std::vector<std::string> invalid_chars{"="s, "*"s, "0G"s};
+    std::vector<std::string> invalid_eof{"0F0"s};
+
+    invalid_input_checks<DecoderTraits, mgs::invalid_input_error>(
+        invalid_chars);
+    invalid_input_checks<DecoderTraits, mgs::unexpected_eof_error>(invalid_eof);
   }
 }
