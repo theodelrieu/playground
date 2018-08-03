a4cd8062d85357cf85db31dd6b8dc9dea8a52092
Théo DELRIEU <theo.delrieu@tanker.io>
2018-08-03 17:43:00 +0200

    wip encoding_traits

---
+++ base_n/include/mgs/base_n/detail/encoding_traits.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <mgs/meta/detected.hpp>
+
+namespace mgs
+{
+inline namespace v1
+{
+namespace base_n
+{
+namespace detail
+{
+// TODO is_encoding_traits, write concept
+}
+}
+}
+}
