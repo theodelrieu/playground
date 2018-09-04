20edd8d47eebdbf595d89684d325450089285cf6
Théo DELRIEU <theo.delrieu@tanker.io>
2018-09-04 16:21:15 +0200

    wip strict_totally_ordered, have to write traits has_*

---
+++ meta/test/test_strict_totally_ordered.cpp
@@ -0,0 +1,14 @@
+#include <catch.hpp>
+
+#include <mgs/meta/concepts/comparison/strict_totally_ordered.hpp>
+#include <mgs/meta/concepts/comparison/strict_totally_ordered_with.hpp>
+#include <mgs/meta/static_asserts.hpp>
+
+#include "meta_test_helpers.hpp"
+
+using namespace mgs::meta;
+namespace comparison_concepts = concepts::comparison;
+
+TEST_CASE("StrictTotallyOrderedWith", "[meta][concepts][comparison]")
+{
+}
