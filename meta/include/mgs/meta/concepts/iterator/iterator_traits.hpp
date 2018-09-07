bf248e822a01f614eeb4bcc009ef41b821742a20
Théo DELRIEU <theo.delrieu@tanker.io>
2018-09-07 15:10:01 +0200

    test iterator_traits

--- meta/include/mgs/meta/concepts/iterator/iterator_traits.hpp
+++ meta/include/mgs/meta/concepts/iterator/iterator_traits.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iterator>
+#include <tuple>
 #include <type_traits>
 
 #include <mgs/meta/detected.hpp>
@@ -20,24 +21,36 @@ namespace concepts
 {
 namespace iterator
 {
+namespace detail
+{
 template <typename T>
-struct is_iterator_traits : std::false_type
+struct is_iterator_traits_impl : std::false_type
 {
 };
 
 template <typename T>
-struct is_iterator_traits<std::iterator_traits<T>>
+struct is_iterator_traits_impl<std::iterator_traits<T>>
 {
-private:
   using traits = std::iterator_traits<T>;
 
-public:
   static constexpr auto value =
       is_detected<detected::types::value_type, traits>::value &&
       is_detected<detected::types::difference_type, traits>::value &&
       is_detected<detected::types::pointer, traits>::value &&
       is_detected<detected::types::iterator_category, traits>::value &&
       is_detected<detected::types::reference, traits>::value;
+};
+}
+
+template <typename T>
+struct is_iterator_traits : detail::is_iterator_traits_impl<T>
+{
+  using requirements = std::tuple<>;
+
+  struct static_assert_t
+  {
+    static_assert(is_iterator_traits::value, "T is not a valid IteratorTraits");
+  };
 };
 
 template <typename T, typename = std::enable_if_t<is_iterator_traits<T>::value>>
---
+++ meta/test/test_iterator_traits.cpp
@@ -0,0 +1,55 @@
+#include <iterator>
+#include <tuple>
+
+#include <catch.hpp>
+
+#include <mgs/meta/concepts/iterator/iterator_traits.hpp>
+#include <mgs/meta/static_asserts.hpp>
+
+#include "meta_test_helpers.hpp"
+
+using namespace mgs::meta;
+namespace iterator_concepts = concepts::iterator;
+
+namespace
+{
+struct invalid_iterator
+{
+};
+
+struct valid_iterator
+{
+};
+}
+
+namespace std
+{
+template <>
+struct iterator_traits<invalid_iterator>
+{
+};
+
+template <>
+struct iterator_traits<valid_iterator>
+{
+  using value_type = int;
+  using pointer = value_type*;
+  using reference = value_type&;
+  using difference_type = std::ptrdiff_t;
+  using iterator_category = std::random_access_iterator_tag;
+};
+}
+
+TEST_CASE("IteratorTraits", "[meta][concepts][iterator]")
+{
+  static_assert(iterator_concepts::is_iterator_traits<std::iterator_traits<char*>>::value, "");
+  static_assert(iterator_concepts::is_iterator_traits<std::iterator_traits<valid_iterator>>::value, "");
+
+  static_assert(!iterator_concepts::is_iterator_traits<std::iterator_traits<invalid_iterator>>::value, "");
+  static_assert(!iterator_concepts::is_iterator_traits<int>::value, "");
+  static_assert(!iterator_concepts::is_iterator_traits<void>::value, "");
+  static_assert(!iterator_concepts::is_iterator_traits<struct incomplete>::value, "");
+
+  generate_failed_requirements_tests<
+      iterator_concepts::is_iterator_traits<int>>(std::tuple<>{});
+}
