6a0887d895de59d962863d4d4bd4cf898cf3f47e
Théo DELRIEU <theo.delrieu@tanker.io>
2018-09-07 16:13:17 +0200

    test iterator

--- meta/include/mgs/meta/concepts/iterator/iterator.hpp
+++ meta/include/mgs/meta/concepts/iterator/iterator.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <iterator>
+#include <tuple>
 #include <type_traits>
 
+#include <mgs/meta/concepts/core/complete_type.hpp>
 #include <mgs/meta/concepts/iterator/iterator_traits.hpp>
 #include <mgs/meta/concepts/iterator/weakly_incrementable.hpp>
 #include <mgs/meta/concepts/object/semiregular.hpp>
@@ -22,14 +24,45 @@ namespace concepts
 {
 namespace iterator
 {
+namespace detail
+{
+template <typename T, typename = void>
+struct is_iterator_impl : std::false_type
+{
+};
+
 template <typename T>
-struct is_iterator
+struct is_iterator_impl<T, std::enable_if_t<core::is_complete_type<T>::value>>
   : std::integral_constant<
         bool,
         is_iterator_traits<std::iterator_traits<T>>::value &&
             is_weakly_incrementable<T>::value &&
             is_detected<detected::operators::dereference, T&>::value>
 {
+};
+}
+
+template <typename T>
+struct is_iterator : detail::is_iterator_impl<T>
+{
+  using requirements = std::tuple<is_iterator_traits<std::iterator_traits<T>>,
+                                  is_weakly_incrementable<T>>;
+
+  struct static_assert_t
+  {
+    static constexpr auto const has_dereference =
+        is_detected<detected::operators::dereference, T&>::value;
+
+    static constexpr int trigger_static_asserts()
+    {
+      static_assert(is_iterator::value, "T is not an Iterator");
+      static_assert(has_dereference,
+                    "Missing or invalid operator: '/* any */ operator*()'");
+      return 0;
+    }
+
+    static constexpr auto _ = trigger_static_asserts();
+  };
 };
 
 template <typename T, typename = std::enable_if_t<is_iterator<T>::value>>
---
+++ meta/test/test_iterator.cpp
@@ -0,0 +1,79 @@
+#include <iterator>
+#include <tuple>
+
+#include <catch.hpp>
+
+#include <mgs/meta/concepts/iterator/iterator.hpp>
+#include <mgs/meta/concepts/iterator/iterator_traits.hpp>
+#include <mgs/meta/concepts/iterator/weakly_incrementable.hpp>
+#include <mgs/meta/static_asserts.hpp>
+
+#include "meta_test_helpers.hpp"
+
+using namespace mgs::meta;
+namespace iterator_concepts = concepts::iterator;
+
+namespace
+{
+struct output_iterator
+{
+  using value_type = void;
+  using pointer = void;
+  using reference = void;
+  using difference_type = void;
+  using iterator_category = std::output_iterator_tag;
+
+  output_iterator& operator++();
+  void operator++(int);
+  void operator*();
+};
+
+struct non_dereferencable_iterator
+{
+  using value_type = void;
+  using pointer = void;
+  using reference = void;
+  using difference_type = void;
+  using iterator_category = std::output_iterator_tag;
+
+  non_dereferencable_iterator& operator++();
+  non_dereferencable_iterator& operator++(int);
+};
+
+struct non_weakly_incrementable_iterator
+{
+  using value_type = void;
+  using pointer = void;
+  using reference = void;
+  using difference_type = void;
+  using iterator_category = std::output_iterator_tag;
+
+  non_weakly_incrementable_iterator& operator*();
+};
+}
+
+TEST_CASE("Iterator", "[meta][concepts][iterator]")
+{
+  static_assert(iterator_concepts::is_iterator<char*>::value, "");
+  static_assert(iterator_concepts::is_iterator<output_iterator>::value, "");
+
+  static_assert(!iterator_concepts::is_iterator<char>::value, "");
+  static_assert(!iterator_concepts::is_iterator_traits<void>::value, "");
+  static_assert(!iterator_concepts::is_iterator_traits<struct incomplete>::value, "");
+
+  static_assert(!iterator_concepts::is_iterator<non_dereferencable_iterator>::value, "");
+  static_assert(!iterator_concepts::is_iterator<non_weakly_incrementable_iterator>::value, "");
+
+  generate_failed_requirements_tests<iterator_concepts::is_iterator<int>>(
+      std::tuple<
+          iterator_concepts::is_iterator_traits<std::iterator_traits<int>>>{});
+
+  generate_failed_requirements_tests<
+      iterator_concepts::is_iterator<non_dereferencable_iterator>>(
+      std::tuple<>{});
+
+  generate_failed_requirements_tests<
+      iterator_concepts::is_iterator<non_weakly_incrementable_iterator>>(
+      std::tuple<iterator_concepts::is_weakly_incrementable<
+          non_weakly_incrementable_iterator>>{});
+}
