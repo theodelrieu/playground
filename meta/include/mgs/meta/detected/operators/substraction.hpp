ac0487b8222f8cf26a685d627cbe048bf17ebdbf
Théo DELRIEU <theo.delrieu@tanker.io>
2018-09-12 12:00:11 +0200

    workaround GCC bug

--- meta/include/mgs/meta/detected/operators/substraction.hpp
+++ meta/include/mgs/meta/detected/operators/substraction.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <type_traits>
 #include <utility>
+
+#include <mgs/meta/detected.hpp>
 
 namespace mgs
 {
@@ -12,8 +15,38 @@ namespace detected
 {
 namespace operators
 {
+namespace detail
+{
 template <typename T, typename U>
 using substraction = decltype(std::declval<T>() - std::declval<U>());
+
+// Workaround a GCC bug with operator-(void*, void*) in unevaluated context
+template <typename T,
+          typename U,
+          typename uncvT = std::remove_cv_t<std::remove_reference_t<T>>,
+          typename uncvU = std::remove_cv_t<std::remove_reference_t<U>>,
+          typename = void>
+struct substraction_impl;
+
+template <typename T, typename U, typename uncvT, typename uncvU>
+struct substraction_impl<T,
+                         U,
+                         uncvT,
+                         uncvU,
+                         std::enable_if_t<!std::is_same<uncvT, void*>::value &&
+                                          !std::is_same<uncvU, void*>::value>>
+{
+  using type = detected_t<substraction, T, U>;
+};
+
+template <typename T, typename U>
+struct substraction_impl<T, U, void*, void*>
+{
+};
+}
+
+template <typename T, typename U>
+using substraction = typename detail::substraction_impl<T, U>::type;
 }
 };
 }
--- meta/include/mgs/meta/detected/operators/substraction_assignment.hpp
+++ meta/include/mgs/meta/detected/operators/substraction_assignment.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <type_traits>
 #include <utility>
+
+#include <mgs/meta/detected.hpp>
 
 namespace mgs
 {
@@ -12,9 +15,41 @@ namespace detected
 {
 namespace operators
 {
+namespace detail
+{
 template <typename T, typename U>
 using substraction_assignment =
     decltype(std::declval<T>() -= std::declval<U>());
+
+// Workaround a GCC bug with operator-=(void*, void*) in unevaluated context
+template <typename T,
+          typename U,
+          typename uncvT = std::remove_cv_t<std::remove_reference_t<T>>,
+          typename uncvU = std::remove_cv_t<std::remove_reference_t<U>>,
+          typename = void>
+struct substraction_assignment_impl;
+
+template <typename T, typename U, typename uncvT, typename uncvU>
+struct substraction_assignment_impl<
+    T,
+    U,
+    uncvT,
+    uncvU,
+    std::enable_if_t<!std::is_same<uncvT, void*>::value &&
+                     !std::is_same<uncvU, void*>::value>>
+{
+  using type = detected_t<substraction_assignment, T, U>;
+};
+
+template <typename T, typename U>
+struct substraction_assignment_impl<T, U, void*, void*>
+{
+};
+}
+
+template <typename T, typename U>
+using substraction_assignment =
+    typename detail::substraction_assignment_impl<T, U>::type;
 }
 };
 }
