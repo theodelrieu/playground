6419a51ae95a97462ce0b190057edac609dde3d4
Théo DELRIEU <theo.delrieu@tanker.io>
2018-09-13 14:57:08 +0200

    test is_detected + fix workarounds

--- meta/include/mgs/meta/detected/operators/dereference.hpp
+++ meta/include/mgs/meta/detected/operators/dereference.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <type_traits>
 #include <utility>
+
+#include <mgs/meta/detected.hpp>
+#include <mgs/meta/void_t.hpp>
 
 namespace mgs
 {
@@ -12,8 +16,34 @@ namespace detected
 {
 namespace operators
 {
+namespace detail
+{
+// Workaround a clang bug with operator*(void*) in unevaluated context
+template <typename T, typename = void>
+struct dereference_impl
+{
+};
+
 template <typename T>
-using dereference = decltype(*std::declval<T&>());
+struct dereference_impl<T, void_t<decltype(*std::declval<T>())>>
+{
+  using type = decltype(*std::declval<T>());
+};
+
+template <typename T, typename>
+struct dereference : dereference_impl<T>
+{
+};
+
+template <typename T>
+struct dereference<T, void*>
+{
+};
+}
+
+template <typename T>
+using dereference = typename detail::
+    dereference<T, std::remove_cv_t<std::remove_reference_t<T>>>::type;
 }
 };
 }
--- meta/include/mgs/meta/detected/operators/substraction.hpp
+++ meta/include/mgs/meta/detected/operators/substraction.hpp
@@ -17,36 +17,38 @@ namespace operators
 {
 namespace detail
 {
-template <typename T, typename U>
-using substraction = decltype(std::declval<T>() - std::declval<U>());
-
 // Workaround a GCC bug with operator-(void*, void*) in unevaluated context
-template <typename T,
-          typename U,
-          typename uncvT = std::remove_cv_t<std::remove_reference_t<T>>,
-          typename uncvU = std::remove_cv_t<std::remove_reference_t<U>>,
-          typename = void>
-struct substraction_impl;
-
-template <typename T, typename U, typename uncvT, typename uncvU>
-struct substraction_impl<T,
-                         U,
-                         uncvT,
-                         uncvU,
-                         std::enable_if_t<!std::is_same<uncvT, void*>::value &&
-                                          !std::is_same<uncvU, void*>::value>>
+template <typename T, typename U, typename = void>
+struct substraction_impl
 {
-  using type = detected_t<substraction, T, U>;
 };
 
 template <typename T, typename U>
-struct substraction_impl<T, U, void*, void*>
+struct substraction_impl<
+    T,
+    U,
+    void_t<decltype(std::declval<T>() - std::declval<U>())>>
+{
+  using type = decltype(std::declval<T>() - std::declval<U>());
+};
+
+template <typename T, typename U, typename, typename>
+struct substraction : substraction_impl<T, U>
+{
+};
+
+template <typename T, typename U>
+struct substraction<T, U, void*, void*>
 {
 };
 }
 
 template <typename T, typename U>
-using substraction = typename detail::substraction_impl<T, U>::type;
+using substraction = typename detail::substraction<
+    T,
+    U,
+    std::remove_cv_t<std::remove_reference_t<T>>,
+    std::remove_cv_t<std::remove_reference_t<U>>>::type;
 }
 };
 }
