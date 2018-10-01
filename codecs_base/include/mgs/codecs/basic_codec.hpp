a2168afd9eacd34418f92137cee0f4052bfba2a7
Théo DELRIEU <theo.delrieu@tanker.io>
2018-10-01 17:49:32 +0200

    wip handle c arrays in basic_codec

--- codecs_base/include/mgs/codecs/basic_codec.hpp
+++ codecs_base/include/mgs/codecs/basic_codec.hpp
@@ -112,6 +112,30 @@ public:
     return basic_codec::encode<T>(begin(it), end(it));
   }
 
+  template <typename T = default_encoded_output,
+            typename U = void,
+            std::size_t N = 0,
+            std::enable_if_t<!std::is_same<std::remove_const_t<U>, char>::value,
+                             int> = 0>
+  static auto encode(U (&tab)[N]) -> decltype(basic_codec::encode<T>(
+      std::declval<meta::result_of_begin<decltype(tab)>>(),
+      std::declval<meta::result_of_end<decltype(tab)>>()))
+  {
+    return basic_codec::encode<T>(std::begin(tab), std::end(tab));
+  }
+
+  template <typename T = default_encoded_output,
+            typename U = void,
+            std::size_t N = 0,
+            std::enable_if_t<std::is_same<std::remove_const_t<U>, char>::value,
+                             int> = 0>
+  static auto encode(U (&tab)[N]) -> decltype(basic_codec::encode<T>(
+      std::declval<meta::result_of_begin<decltype(tab)>>(),
+      std::declval<meta::result_of_end<decltype(tab)>>()))
+  {
+    return basic_codec::encode<T>(std::string(tab));
+  }
+
   template <typename T = default_decoded_output,
             typename Iterable = void,
             typename = std::enable_if_t<
@@ -124,6 +148,30 @@ public:
     using std::end;
 
     return basic_codec::decode<T>(begin(it), end(it));
+  }
+
+  template <typename T = default_encoded_output,
+            typename U = void,
+            std::size_t N = 0,
+            std::enable_if_t<!std::is_same<std::remove_const_t<U>, char>::value,
+                             int> = 0>
+  static auto decode(U (&tab)[N]) -> decltype(basic_codec::decode<T>(
+      std::declval<meta::result_of_begin<decltype(tab)>>(),
+      std::declval<meta::result_of_end<decltype(tab)>>()))
+  {
+    return basic_codec::decode<T>(std::begin(tab), std::end(tab));
+  }
+
+  template <typename T = default_decoded_output,
+            typename U = void,
+            std::size_t N = 0,
+            std::enable_if_t<std::is_same<std::remove_const_t<U>, char>::value,
+                             int> = 0>
+  static auto decode(U (&tab)[N]) -> decltype(basic_codec::decode<T>(
+      std::declval<meta::result_of_begin<decltype(tab)>>(),
+      std::declval<meta::result_of_end<decltype(tab)>>()))
+  {
+    return basic_codec::decode<T>(std::string(tab));
   }
 };
 }
