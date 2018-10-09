f52e9404e77ab23632f4a1748ce6ebfd4fe65583
Théo DELRIEU <theo.delrieu@tanker.io>
2018-10-09 18:10:07 +0200

    todo move things in detail

--- binary_to_text/include/mgs/codecs/binary_to_text/basic_encoder.hpp
+++ binary_to_text/include/mgs/codecs/binary_to_text/basic_encoder.hpp
@@ -104,55 +104,6 @@ struct output_fast_encoder<EncodingTraits, 3, 4>
     out[3] = EncodingTraits::alphabet[((input_bits & mask).to_ulong())];
   }
 };
-
-template <typename EncodingTraits>
-struct find_good_name
-{
-  using BitshiftTraits = bitshift_traits<EncodingTraits::nb_input_bytes,
-                                         EncodingTraits::nb_output_bytes>;
-
-  template <typename T>
-  static void read_input(T const& input, static_vector<char, 256>& output)
-  {
-    static std::bitset<BitshiftTraits::nb_total_input_bits> const mask(
-        pow<2, BitshiftTraits::nb_encoded_bits>() - 1);
-
-    auto const nb_loop_iterations =
-        std::ldiv(input.size(), BitshiftTraits::nb_input_bytes);
-    output.resize(nb_loop_iterations.quot * BitshiftTraits::nb_output_bytes);
-
-    for (auto i = 0u; i < nb_loop_iterations.quot; ++i)
-    {
-      auto const input_bits = input_to_bitset<BitshiftTraits>(
-          input.begin() + (i * BitshiftTraits::nb_input_bytes),
-          BitshiftTraits::nb_input_bytes);
-
-      output_fast_encoder<EncodingTraits>::encode(
-          input_bits, output.begin() + (i * BitshiftTraits::nb_output_bytes));
-    }
-
-    if (nb_loop_iterations.rem)
-    {
-      auto const nb_non_padded_bytes =
-          ((8 * (nb_loop_iterations.rem)) / BitshiftTraits::nb_encoded_bits) +
-          1;
-      auto const nb_padded_bytes =
-          BitshiftTraits::nb_output_bytes - nb_non_padded_bytes;
-
-      auto const input_bits = input_to_bitset<BitshiftTraits>(
-          input.begin() +
-              (nb_loop_iterations.quot * BitshiftTraits::nb_input_bytes),
-          nb_loop_iterations.rem);
-
-      auto const old_size = output.size();
-      output.resize(output.size() + nb_padded_bytes + nb_non_padded_bytes);
-      output_slow_encoder<EncodingTraits>::encode(
-          input_bits, output.begin() + old_size, nb_non_padded_bytes);
-      padding_writer<EncodingTraits>::write(output.end() - nb_padded_bytes,
-                                            nb_padded_bytes);
-    }
-  }
-};
 }
 
 template <typename Iterator, typename Sentinel, typename EncodingTraits>
@@ -170,33 +121,20 @@ private:
   static_assert(EncodingTraits::padding_policy != padding_policy::optional,
                 "optional padding does not make sense when encoding");
 
-  static constexpr auto nb_output_bytes = EncodingTraits::nb_output_bytes;
-  static constexpr auto nb_input_bytes = EncodingTraits::nb_input_bytes;
+  using BitshiftTraits =
+      detail::bitshift_traits<EncodingTraits::nb_input_bytes,
+                              EncodingTraits::nb_output_bytes>;
 
-  static constexpr auto nb_total_input_bits =
-      detail::round_to_multiple_of<nb_input_bytes * 8, nb_output_bytes>();
-  static constexpr auto nb_extra_input_bits = nb_total_input_bits % 8;
-  static constexpr auto nb_encoded_bits = nb_total_input_bits / nb_output_bytes;
-
-  static_assert(nb_total_input_bits % nb_output_bytes == 0,
+  static_assert(BitshiftTraits::nb_total_input_bits % BitshiftTraits::nb_output_bytes == 0,
                 "The impossible has occurred");
-  static_assert(detail::pow<2, nb_encoded_bits>() ==
+  static_assert(detail::pow<2, BitshiftTraits::nb_encoded_bits>() ==
                     sizeof(EncodingTraits::alphabet),
                 "Alphabet size must be a power of 2");
-  static_assert(nb_output_bytes % 2 == 0,
+  static_assert(BitshiftTraits::nb_output_bytes % 2 == 0,
                 "nb_output_bytes must be a power of two");
-  static_assert(nb_output_bytes <= 8, "nb_output_bytes must be <= 8");
 
   static constexpr auto nb_bytes_to_read =
-      (256 / nb_output_bytes) * nb_input_bytes;
-
-  // TODO
-  // nb_output_bytes must be a power of 2
-  // (256 / nb_output_bytes) * nb_input_bytes = nb_input_bytes_to_read
-  // assert nb_output_bytes <= 8
-  // nb read_bytes per loop + nb bytes to encode per loop: *= 8 /
-  // nb_output_bytes
-
+      (256 / BitshiftTraits::nb_output_bytes) * BitshiftTraits::nb_input_bytes;
 
 public:
   using value_type = detail::static_vector<char, 256>;
@@ -250,7 +188,45 @@ private:
         _end,
         typename std::iterator_traits<Iterator>::iterator_category{});
 
-    detail::find_good_name<EncodingTraits>::read_input(input, output);
+    {
+      constexpr std::bitset<BitshiftTraits::nb_total_input_bits> mask(
+          detail::pow<2, BitshiftTraits::nb_encoded_bits>() - 1);
+
+      auto const nb_loop_iterations =
+          std::ldiv(input.size(), BitshiftTraits::nb_input_bytes);
+      output.resize(nb_loop_iterations.quot * BitshiftTraits::nb_output_bytes);
+
+      for (auto i = 0u; i < nb_loop_iterations.quot; ++i)
+      {
+        auto const input_bits = detail::input_to_bitset<BitshiftTraits>(
+            input.begin() + (i * BitshiftTraits::nb_input_bytes),
+            BitshiftTraits::nb_input_bytes);
+
+        detail::output_fast_encoder<EncodingTraits>::encode(
+            input_bits, output.begin() + (i * BitshiftTraits::nb_output_bytes));
+      }
+
+      if (nb_loop_iterations.rem)
+      {
+        auto const nb_non_padded_bytes =
+            ((8 * (nb_loop_iterations.rem)) / BitshiftTraits::nb_encoded_bits) +
+            1;
+        auto const nb_padded_bytes =
+            BitshiftTraits::nb_output_bytes - nb_non_padded_bytes;
+
+        auto const input_bits = detail::input_to_bitset<BitshiftTraits>(
+            input.begin() +
+                (nb_loop_iterations.quot * BitshiftTraits::nb_input_bytes),
+            nb_loop_iterations.rem);
+
+        auto const old_size = output.size();
+        output.resize(output.size() + nb_padded_bytes + nb_non_padded_bytes);
+        detail::output_slow_encoder<EncodingTraits>::encode(
+            input_bits, output.begin() + old_size, nb_non_padded_bytes);
+        detail::padding_writer<EncodingTraits>::write(
+            output.end() - nb_padded_bytes, nb_padded_bytes);
+      }
+    }
   }
 
   template <typename I, typename S, typename T>
