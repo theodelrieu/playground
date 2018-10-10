e8b23defd03b317cff8fd5cc5cbf3bebbf640758
Théo DELRIEU <theo.delrieu@tanker.io>
2018-10-10 17:02:30 +0200

    fix

--- binary_to_text/include/mgs/codecs/binary_to_text/basic_encoder.hpp
+++ binary_to_text/include/mgs/codecs/binary_to_text/basic_encoder.hpp
@@ -53,11 +53,6 @@ auto input_to_bitset(RandomAccessIterator it, std::size_t n)
   return input_bits;
 }
 
-template <typename EncodingTraits,
-          std::size_t InputBytes = EncodingTraits::nb_input_bytes,
-          std::size_t OutputBytes = EncodingTraits::nb_output_bytes>
-struct output_fast_encoder;
-
 template <typename EncodingTraits>
 struct output_slow_encoder
 {
@@ -70,7 +65,7 @@ struct output_slow_encoder
       OutputIterator out,
       std::size_t n)
   {
-    static std::bitset<BitshiftTraits::nb_total_input_bits> const mask(
+    constexpr std::bitset<BitshiftTraits::nb_total_input_bits> mask(
         pow<2, BitshiftTraits::nb_encoded_bits>() - 1);
 
     for (auto i = 0u; i < BitshiftTraits::nb_output_bytes; ++i)
@@ -82,6 +77,22 @@ struct output_slow_encoder
               .to_ulong();
       out[i] = EncodingTraits::alphabet[index];
     }
+  }
+};
+
+template <typename EncodingTraits,
+          std::size_t InputBytes = EncodingTraits::nb_input_bytes,
+          std::size_t OutputBytes = EncodingTraits::nb_output_bytes>
+struct output_fast_encoder
+{
+  using BitshiftTraits = bitshift_traits<InputBytes, OutputBytes>;
+
+  template <typename OutputIterator>
+  static void encode(
+      std::bitset<BitshiftTraits::nb_total_input_bits> const& input_bits,
+      OutputIterator out)
+  {
+    output_slow_encoder<EncodingTraits>::encode(input_bits, out, OutputBytes);
   }
 };
 
@@ -188,44 +199,42 @@ private:
         _end,
         typename std::iterator_traits<Iterator>::iterator_category{});
 
+    constexpr std::bitset<BitshiftTraits::nb_total_input_bits> mask(
+        detail::pow<2, BitshiftTraits::nb_encoded_bits>() - 1);
+
+    auto const nb_loop_iterations =
+        std::ldiv(input.size(), BitshiftTraits::nb_input_bytes);
+    output.resize(nb_loop_iterations.quot * BitshiftTraits::nb_output_bytes);
+
+    for (auto i = 0u; i < nb_loop_iterations.quot; ++i)
     {
-      constexpr std::bitset<BitshiftTraits::nb_total_input_bits> mask(
-          detail::pow<2, BitshiftTraits::nb_encoded_bits>() - 1);
+      auto const input_bits = detail::input_to_bitset<BitshiftTraits>(
+          input.begin() + (i * BitshiftTraits::nb_input_bytes),
+          BitshiftTraits::nb_input_bytes);
 
-      auto const nb_loop_iterations =
-          std::ldiv(input.size(), BitshiftTraits::nb_input_bytes);
-      output.resize(nb_loop_iterations.quot * BitshiftTraits::nb_output_bytes);
+      detail::output_fast_encoder<EncodingTraits>::encode(
+          input_bits, output.begin() + (i * BitshiftTraits::nb_output_bytes));
+    }
 
-      for (auto i = 0u; i < nb_loop_iterations.quot; ++i)
-      {
-        auto const input_bits = detail::input_to_bitset<BitshiftTraits>(
-            input.begin() + (i * BitshiftTraits::nb_input_bytes),
-            BitshiftTraits::nb_input_bytes);
+    if (nb_loop_iterations.rem)
+    {
+      auto const nb_non_padded_bytes =
+          ((8 * (nb_loop_iterations.rem)) / BitshiftTraits::nb_encoded_bits) +
+          1;
+      auto const nb_padded_bytes =
+          BitshiftTraits::nb_output_bytes - nb_non_padded_bytes;
 
-        detail::output_fast_encoder<EncodingTraits>::encode(
-            input_bits, output.begin() + (i * BitshiftTraits::nb_output_bytes));
-      }
+      auto const input_bits = detail::input_to_bitset<BitshiftTraits>(
+          input.begin() +
+              (nb_loop_iterations.quot * BitshiftTraits::nb_input_bytes),
+          nb_loop_iterations.rem);
 
-      if (nb_loop_iterations.rem)
-      {
-        auto const nb_non_padded_bytes =
-            ((8 * (nb_loop_iterations.rem)) / BitshiftTraits::nb_encoded_bits) +
-            1;
-        auto const nb_padded_bytes =
-            BitshiftTraits::nb_output_bytes - nb_non_padded_bytes;
-
-        auto const input_bits = detail::input_to_bitset<BitshiftTraits>(
-            input.begin() +
-                (nb_loop_iterations.quot * BitshiftTraits::nb_input_bytes),
-            nb_loop_iterations.rem);
-
-        auto const old_size = output.size();
-        output.resize(output.size() + nb_padded_bytes + nb_non_padded_bytes);
-        detail::output_slow_encoder<EncodingTraits>::encode(
-            input_bits, output.begin() + old_size, nb_non_padded_bytes);
-        detail::padding_writer<EncodingTraits>::write(
-            output.end() - nb_padded_bytes, nb_padded_bytes);
-      }
+      auto const old_size = output.size();
+      output.resize(output.size() + nb_padded_bytes + nb_non_padded_bytes);
+      detail::output_slow_encoder<EncodingTraits>::encode(
+          input_bits, output.begin() + old_size, nb_non_padded_bytes);
+      detail::padding_writer<EncodingTraits>::write(
+          output.end() - nb_padded_bytes, nb_padded_bytes);
     }
   }
 
