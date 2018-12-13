1bf3cb00a518e1335af326d1a1269a2f5c8f5789
Théo DELRIEU <theo.delrieu@tanker.io>
2018-12-13 16:28:25 +0100

    concept SizedTransformedInputAdapter

---
+++ adapters/include/mgs/adapters/concepts/sized_transformed_input_adapter.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <cstddef>
+#include <tuple>
+
+#include <mgs/adapters/concepts/transformed_input_adapter.hpp>
+#include <mgs/adapters/detail/detected/member_functions/max_transformed_size.hpp>
+#include <mgs/meta/detected.hpp>
+
+// template <typename T>
+// concept SizedTransformedInputAdapter = TransformedInputAdapter<T> &&
+//   requires (T const& v) {
+//     { v.max_transformed_size() } -> std::size_t
+//   }
+
+namespace mgs
+{
+inline namespace v1
+{
+namespace adapters
+{
+namespace concepts
+{
+template <typename T>
+struct is_sized_transformed_input_adapter
+{
+private:
+  static constexpr auto const has_max_transformed_size = meta::is_detected_exact<
+      std::size_t,
+      detail::detected::member_functions::max_transformed_size,
+      T const&>::value;
+
+public:
+  using requirements = std::tuple<is_transformed_input_adapter<T>>;
+
+  static constexpr auto const value =
+      is_transformed_input_adapter<T>::value && has_max_transformed_size;
+
+  constexpr int trigger_static_asserts()
+  {
+    static_assert(value, "T is not a SizedTransformedInputAdapter");
+    static_assert(has_max_transformed_size,
+                  "Missing or invalid function: 'std::size_t "
+                  "T::max_transformed_size() const'");
+    return 1;
+  }
+};
+}
+}
+}
+}
