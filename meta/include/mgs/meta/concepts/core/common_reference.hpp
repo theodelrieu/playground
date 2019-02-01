fd3d7f377b3e9dff4eb7af2793ced673792f3b2a
Théo DELRIEU <theo.delrieu@tanker.io>
2019-02-01 14:30:34 +0100

    wip

--- meta/include/mgs/meta/concepts/core/common_reference.hpp
+++ meta/include/mgs/meta/concepts/core/common_reference.hpp
@@ -6,6 +6,7 @@
 #include <mgs/meta/common_reference.hpp>
 #include <mgs/meta/concepts/core/complete_type.hpp>
 #include <mgs/meta/concepts/core/convertible_to.hpp>
+#include <mgs/meta/detected.hpp>
 #include <mgs/meta/void_t.hpp>
 
 // https://en.cppreference.com/w/cpp/experimental/ranges/concepts/CommonReference
@@ -25,22 +26,56 @@ namespace detail
 template <typename T, typename U, typename = void>
 struct has_common_reference_impl : std::false_type
 {
+  static constexpr int trigger_static_asserts()
+  {
+    static_assert(is_detected<common_reference_t, T, U>::value,
+                  "T and U do not share a common reference type");
+    static_assert(is_detected<common_reference_t, U, T>::value,
+                  "U and T do not share a common reference type");
+    return 1;
+  }
 };
 
 template <typename T, typename U>
 struct has_common_reference_impl<
     T,
     U,
-    void_t<meta::common_reference_t<T, U>, meta::common_reference_t<U, T>>>
+    void_t<common_reference_t<T, U>, common_reference_t<U, T>>>
 {
-  // TODO convertible_to
+private:
+  using CommonRefTU = common_reference_t<T, U>;
+  using CommonRefUT = common_reference_t<U, T>;
+
+public:
   static constexpr auto const value =
-      std::is_same<meta::common_reference_t<T, U>,
-                   meta::common_reference_t<U, T>>::value &&
-      is_convertible_to<T, meta::common_reference_t<T, U>>::value &&
-      i<U, meta::common_reference_t<T, U>>::value;
+      std::is_same<CommonRefTU, CommonRefUT>::value &&
+      is_convertible_to<T, CommonRefTU>::value &&
+      is_convertible_to<U, CommonRefTU>::value;
+
+  static constexpr int trigger_static_asserts()
+  {
+    static_assert(std::is_same<CommonRefTU, CommonRefUT>::value,
+                  "common_reference_t<T, U> must be the same then "
+                  "common_reference_t<U, T>");
+    static_assert(is_convertible_to<T, CommonRefTU>::value,
+                  "T must be convertible to common_reference_t<T, U>");
+    static_assert(is_convertible_to<U, CommonRefTU>::value,
+                  "U must be convertible to common_reference_t<T, U>");
+    return 1;
+  }
 };
 }
+
+template <typename T, typename U>
+struct has_common_reference : detail::has_common_reference_impl<T, U>
+{
+  using requirements = std::tuple<>;
+};
+
+template <typename T,
+          typename U,
+          typename = std::enable_if_t<has_common_reference<T, U>::value>>
+using CommonReference = T;
 }
 }
 }
--- meta/test/test_common_reference.cpp
+++ meta/test/test_common_reference.cpp
@@ -4,10 +4,12 @@
 #include <catch.hpp>
 
 #include <mgs/meta/common_reference.hpp>
+#include <mgs/meta/concepts/core/common_reference.hpp>
 #include <mgs/meta/detected.hpp>
-#include <mgs/meta/detected/types/type.hpp>
 
 using namespace mgs::meta;
+
+namespace core_concepts = concepts::core;
 
 // Use a part of the range-v3 test suite for this specific trait:
 namespace 
@@ -20,66 +22,90 @@ struct D : B
 
 TEST_CASE("CommonReference", "[meta][concepts][core]")
 {
-  static_assert(!is_detected<detected::types::type, common_reference<>>::value,
+  static_assert(!is_detected<common_reference_t>::value,
                 "");
-  static_assert(!is_detected<detected::types::type, common_reference<int, void const>>::value,
-                "");
-  static_assert(!is_detected<detected::types::type, common_reference<int, char[]>>::value,
-                "");
-  static_assert(!is_detected<detected::types::type,
-                             common_reference<int, struct incomplete>>::value,
-                "");
+  static_assert(!is_detected<common_reference_t, int, void const>::value, "");
+  static_assert(!is_detected<common_reference_t, int, char(&)[]>::value, "");
+  static_assert(
+      !is_detected<common_reference_t, int, struct incomplete*>::value, "");
 
-  static_assert(std::is_same<common_reference<int>::type, int>::value, "");
-  static_assert(std::is_same<common_reference<int, int>::type, int>::value, "");
-  static_assert(std::is_same<common_reference<int, int, int>::type, int>::value, "");
+  static_assert(std::is_same<common_reference_t<int>, int>::value, "");
+  static_assert(std::is_same<common_reference_t<int, int>, int>::value, "");
+  static_assert(std::is_same<common_reference_t<int, int, int>, int>::value,
+                "");
+  static_assert(core_concepts::has_common_reference<int, int>::value, "");
 
-  static_assert(std::is_same<common_reference<int const&, int volatile&>::type,
+  static_assert(std::is_same<common_reference_t<int const&, int volatile&>,
                              int const volatile&>::value,
                 "");
+  static_assert(
+      core_concepts::has_common_reference<int const&, int volatile&>::value,
+      "");
 
   static_assert(std::is_same<common_reference_t<B&, D&>, B&>::value, "");
+  static_assert(core_concepts::has_common_reference<B&, D&>::value, "");
   static_assert(std::is_same<common_reference_t<B&, D const&>, B const&>::value,
                 "");
+  static_assert(core_concepts::has_common_reference<B&, D const&>::value, "");
   static_assert(
       std::is_same<common_reference_t<B&, D const&, D&>, B const&>::value, "");
   static_assert(std::is_same<common_reference_t<B const&, D&>, B const&>::value,
                 "");
+  static_assert(core_concepts::has_common_reference<B const&, D&>::value, "");
   static_assert(std::is_same<common_reference_t<B&, D&, B&, D&>, B&>::value,
                 "");
 
   static_assert(std::is_same<common_reference_t<B&&, D&&>, B&&>::value, "");
+  static_assert(core_concepts::has_common_reference<B&&, D&&>::value, "");
   static_assert(
       std::is_same<common_reference_t<B const&&, D&&>, B const&&>::value, "");
+  static_assert(core_concepts::has_common_reference<B const&&, D&&>::value, "");
   static_assert(
       std::is_same<common_reference_t<B&&, D const&&>, B const&&>::value, "");
+  static_assert(core_concepts::has_common_reference<B&&, D const&&>::value, "");
 
   static_assert(std::is_same<common_reference_t<B&, D&&>, B const&>::value, "");
+  static_assert(core_concepts::has_common_reference<B&, D&&>::value, "");
   static_assert(
       std::is_same<common_reference_t<B&, D const&&>, B const&>::value, "");
+  static_assert(core_concepts::has_common_reference<B&, D const&&>::value, "");
   static_assert(
       std::is_same<common_reference_t<B const&, D&&>, B const&>::value, "");
+  static_assert(core_concepts::has_common_reference<B const&, D&&>::value, "");
 
   static_assert(std::is_same<common_reference_t<B&&, D&>, B const&>::value, "");
+  static_assert(core_concepts::has_common_reference<B&&, D&>::value, "");
   static_assert(
       std::is_same<common_reference_t<B&&, D const&>, B const&>::value, "");
+  static_assert(core_concepts::has_common_reference<B&&, D const&>::value, "");
   static_assert(
       std::is_same<common_reference_t<B const&&, D&>, B const&>::value, "");
+  static_assert(core_concepts::has_common_reference<B const&&, D&>::value, "");
 
   static_assert(std::is_same<common_reference_t<int, short>, int>::value, "");
   static_assert(std::is_same<common_reference_t<int, short&>, int>::value, "");
   static_assert(std::is_same<common_reference_t<int&, short&>, int>::value, "");
   static_assert(std::is_same<common_reference_t<int&, short>, int>::value, "");
+  static_assert(core_concepts::has_common_reference<int, short>::value, "");
+  static_assert(core_concepts::has_common_reference<int, short&>::value, "");
+  static_assert(core_concepts::has_common_reference<int&, short&>::value, "");
+  static_assert(core_concepts::has_common_reference<int&, short>::value, "");
 
   // tricky volatile reference case
   static_assert(
       std::is_same<common_reference_t<int&&, int volatile&>, int>::value, "");
+  static_assert(core_concepts::has_common_reference<int&&, int volatile&>::value, "");
   static_assert(
       std::is_same<common_reference_t<int volatile&, int&&>, int>::value, "");
+  static_assert(
+      core_concepts::has_common_reference<int volatile&, int&&>::value, "");
   static_assert(
       std::is_same<common_reference_t<int const volatile&&, int volatile&&>,
                    int const volatile&&>::value,
       "");
+  static_assert(core_concepts::has_common_reference<int const volatile&&,
+                                                    int volatile&&>::value,
+                "");
   static_assert(
       std::is_same<common_reference_t<int&&, int const&, int volatile&>,
                    int const volatile&>(),
@@ -90,10 +116,18 @@ TEST_CASE("CommonReference", "[meta][concepts][core]")
                              int const(&)[10]>::value,
                 "");
   static_assert(
+      core_concepts::has_common_reference<int(&)[10], int(&&)[10]>::value, "");
+  static_assert(
       std::is_same<common_reference_t<int const(&)[10], int volatile(&)[10]>,
                    int const volatile(&)[10]>::value,
       "");
   static_assert(
+      core_concepts::has_common_reference<int const(&)[10],
+                                          int volatile(&&)[10]>::value,
+      "");
+  static_assert(
       std::is_same<common_reference_t<int(&)[10], int(&)[11]>, int*>::value,
       "");
+  static_assert(
+      core_concepts::has_common_reference<int(&)[10], int(&)[11]>::value, "");
 }
