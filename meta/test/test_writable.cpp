573fa4979ee4506cc38719bd0f422f86029ec98d
Théo DELRIEU <theo.delrieu@tanker.io>
2019-02-04 15:33:40 +0100

    wip

---
+++ meta/test/test_writable.cpp
@@ -0,0 +1,88 @@
+#include <iterator>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <catch.hpp>
+
+#include <mgs/meta/concepts/iterator/writable.hpp>
+#include <mgs/meta/static_asserts.hpp>
+
+#include <test_helpers/requirements.hpp>
+
+using namespace mgs::meta;
+namespace iterator_concepts = concepts::iterator;
+
+namespace
+{
+struct It
+{
+  using value_type = std::string;
+  using pointer = value_type*;
+  using reference = value_type;
+  using difference_type = std::ptrdiff_t;
+  using iterator_category = std::input_iterator_tag;
+
+  reference operator*();
+  It& operator++();
+  It operator++(int);
+};
+
+// std::vector<bool>::iterator should be Writable, however the proxy it uses
+// usually does not have const/rvalue overloads for operator=.
+// Therefore write our own 'correct' proxy class.
+struct It2
+{
+  struct Proxy
+  {
+    Proxy operator*() const;
+
+    Proxy& operator=(std::string const&&) const;
+    Proxy& operator=(std::string&&) const;
+    Proxy& operator=(std::string const&) const;
+    Proxy& operator=(std::string&) const;
+
+    Proxy& operator=(Proxy const&) = default;
+    Proxy& operator=(Proxy&&) = default;
+
+    operator std::string() const;
+
+    // Imagine having a std::string* as member
+  };
+
+  using value_type = std::string;
+  using pointer = Proxy*;
+  using reference = Proxy;
+  using difference_type = std::ptrdiff_t;
+  using iterator_category = std::input_iterator_tag;
+
+  reference operator*();
+  It& operator++();
+  It operator++(int);
+};
+}
+
+TEST_CASE("Writable", "[meta][concepts][iterator]")
+{
+  static_assert(!iterator_concepts::is_writable<void, int>::value, "");
+  static_assert(!iterator_concepts::is_writable<void*, int>::value, "");
+  static_assert(!iterator_concepts::is_writable<struct incomplete*, int>::value, "");
+  static_assert(!iterator_concepts::is_writable<struct incomplete, int>::value, "");
+
+  static_assert(!iterator_concepts::is_writable<It, std::string>::value, "");
+  static_assert(iterator_concepts::is_writable<It2, std::string>::value, "");
+
+  static_assert(iterator_concepts::is_writable<char*, char>::value, "");
+  static_assert(iterator_concepts::is_writable<char* const, char>::value, "");
+  static_assert(!iterator_concepts::is_writable<char const*, char>::value, "");
+  static_assert(iterator_concepts::is_writable<std::ostreambuf_iterator<char>,
+                                               char>::value,
+                "");
+  static_assert(iterator_concepts::is_writable<
+                    std::back_insert_iterator<std::vector<int>>,
+                    int const&>::value,
+                "");
+  static_assert(
+      iterator_concepts::is_writable<std::unique_ptr<float>, int const&>::value,
+      "");
+}
