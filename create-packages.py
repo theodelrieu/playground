60da55511b6fe37b50b87bf8fd2e980544ecb0e6
Théo DELRIEU <theo.delrieu@tanker.io>
2019-07-05 14:01:05 +0200

    wip

--- create-packages.py
+++ create-packages.py
@@ -31,6 +31,7 @@ def run_conan(*args):
 def main():
     for pkg in PACKAGES:
         run_conan("create", pkg, "mgs/testing", "--build", "mgs_%s" % pkg, "-s", "cppstd=14")
+    run_conan("create", "all", "mgs/testing", "--build", "mgs", "-s", "cppstd=14")
 
 
 if __name__ == "__main__":
