091ae46c30e03f60e74abd3844e99d7d66829b61
Théo DELRIEU <theo.delrieu@tanker.io>
2019-10-22 17:42:24 +0200

    input_iterator

--- meta/include/mgs/meta/concepts/input_iterator.hpp
+++ meta/include/mgs/meta/concepts/input_iterator.hpp
@@ -44,6 +44,6 @@ template <typename T>
 constexpr auto is_input_iterator_v = is_input_iterator<T>::value;
 
 template <typename T, typename = std::enable_if_t<is_input_iterator<T>::value>>
-using InputIterator = T;
+using input_iterator = T;
 }
 }
